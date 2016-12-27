cf3d951d21b2e9eee23dafd81e4761065d3e2bc5
fallen <fengzilijie@gmail.com>
2016-12-27 16:30:37 +0800

    +

---
+++ src/excel/ExcelReaderTest.java
@@ -0,0 +1,23 @@
+package excel;
+
+import junit.framework.TestCase;
+
+public class ExcelReaderTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testGetCellDataIntInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCellDataIntString() {
+		fail("Not yet implemented");
+	}
+
+}
